1852fa009605120cfd9f5a1b9c904d0f6fda59cc
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2025-07-18 23:06:15 +0530

    updated code

--- src/components/Rightside.tsx
+++ src/components/Rightside.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { myApiKey } from "../config";
-import Star from "./StarRating";
 import StarRating from "./StarRating";
 
 interface RightSideProps {
