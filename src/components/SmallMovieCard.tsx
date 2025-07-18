e7938b235736d577e7aa465b7876601cec2a0064
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2025-07-18 21:59:21 +0530

    updated smallMovieCard

--- src/components/SmallMovieCard.tsx
+++ src/components/SmallMovieCard.tsx
@@ -2,15 +2,21 @@ interface SmallMoviesCardInterface {
   title: string;
   year: string;
   imageId: string;
-  rating?: string;
+  imbId?: string;
+  onCardClick :(id :string)=> void 
 }
-const SmallMovieCard = ({ title, year, imageId,rating }: SmallMoviesCardInterface) => {
+const SmallMovieCard = ({ title, year, imageId,imbId ,onCardClick }: SmallMoviesCardInterface) => {
+
   const smallTitle = title.split(" ")[0];
   const alternateImage =
     "https://plus.unsplash.com/premium_photo-1670210080045-a2e0da63dd99?q=80&w=1315&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   return (
     <div>
-      <div className="flex border-b border-blue-950 p-2 gap-30 m-1 scroll-m-0 hover:bg-indigo-300/20 rounded ">
+      <div onClick={()=>{
+       if(imbId){
+        onCardClick(imbId)
+       }
+      }} className="flex border-b  border-blue-950 p-2 gap-10 m-1 scroll-m-0 hover:bg-indigo-300/20 rounded ">
         <div className="w-16">
           <img
             src={imageId === "N/A" ? alternateImage : imageId}
@@ -21,7 +27,7 @@ const SmallMovieCard = ({ title, year, imageId,rating }: SmallMoviesCardInterfac
         <div className="text-center">
           <h1 className="font-semibold ">{title}</h1>
           <h2>{year}</h2>
-\        </div>
+        </div>
       </div>
     </div>
   );
